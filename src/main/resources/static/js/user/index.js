854de3746660288d69f7235580dea6992678e2c0
RuiBin <2770471187@qq.com>
2019-06-19 17:51:50 +0800

    date：2019-6-19 17:51:48

--- src/main/resources/static/js/user/index.js
+++ src/main/resources/static/js/user/index.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-	$("input[value=QQ]").click(function(){
-		$.message({
-			type:"info",
-			message:"text"
+	$("input[value=QQ]").click(function() {
+		// $.message({
+		// type:"info",
+		// message:"text"
+		// });
+
+		var html = "<div id='showTips'><a>!</a><span>zhansgan</span></div>";
+		$("body").append(html);
+		$("#showTips").css({
+			"width" : "400px",
+			"height" : "55px",
+			"left" : "50%",
+			"top" : "20%",
+			"text-indent" : "",
+			"line-height" : "55px",
+			"transform" : "translateX(-50%)",
+			"position" : "fixed",
+			"z-index" : "2000"
 		});
+		$("#showTips").css({{"background-color":"#F0F9EB;");
 	});
 });
